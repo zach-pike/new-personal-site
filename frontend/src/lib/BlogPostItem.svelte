cb5f4558b03f4a45b8b732e21c4b978a82ad3df8
Zachary Pike <zachturnpike@gmail.com>
2023-08-04 18:02:07 -0400

    Render md

--- frontend/src/lib/BlogPostItem.svelte
+++ frontend/src/lib/BlogPostItem.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
+    import { marked } from "marked";
+
     export let title: string;
     export let poster: string;
     export let post_content: string;
     export let date_of_creation: number;
-    export let uuid: string;
+    export let uuid: string; uuid;
 
     let date = new Date(date_of_creation).toLocaleString();
 </script>
 
 
-<div class="w-3/4">
+<div>
     <div class="p-1">
         <p class="text-2xl font-bold">{title}</p>
-        <p class="text-sm italic">By: {uuid} at {date}</p>
+        <p class="text-sm italic">By: {poster} at {date}</p>
     </div>
 
     <div class="w-full h-[2px] bg-gray-400"></div>
 
+    <div class="prose m-2 w-full">
+        {@html marked.parse(post_content)}
+    </div>
 </div>
+
+<style>
+</style>
