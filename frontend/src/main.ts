e2fb862885de01da48084683755ce37c2193b3ab
Zachary Pike <zachturnpike@gmail.com>
2023-08-08 15:23:43 -0400

    Added some code to stop warns from marked

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,18 @@
 import App from './App.svelte'
 import './index.css';
 
+import { marked } from "marked";
+import { gfmHeadingId } from "marked-gfm-heading-id";
+import { mangle } from "marked-mangle";
+
+
+const options = {
+	prefix: "home-blog-",
+};
+
+marked.use(gfmHeadingId(options));
+marked.use(mangle());
+
 const app = new App({
   target: document.getElementById('app'),
 })
