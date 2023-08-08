dafc3cc8256737874dbe05761dfe9c423dfec4d8
Zachary Pike <zachturnpike@gmail.com>
2023-08-08 15:30:08 -0400

    Add Admin module for posting blogs

---
+++ frontend/src/pages/admin_modules/CreateBlogPost.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import TextInput from "../../lib/TextInput.svelte";
+</script>
+<div class="w-1/2 h-[35em]">
+    <p class="text-xl">Create a new blog post</p>
+    <form>
+        <TextInput placeholder="Post title" />
+        <textarea placeholder="Post content (supports MD)" class="outline-0 p-1 rounded border-2 border-gray-400 focus:border-blue-500 w-full max-h-full"></textarea>
+    </form>
+</div>
